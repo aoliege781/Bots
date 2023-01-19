a8a4f24b2b7a5d5254507e9138192e8503870190
aoliege781 <olegoleg781@yahoo.com>
2023-01-19 04:14:27 +0300

    Update main.py

--- Lexico/main.py
+++ Lexico/main.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 from class_player import Player
 from class_card import Card
@@ -15,26 +16,55 @@ def getFile(c):
                 break
     return word_list
 
+# get path to file and list with info
+# return - nothing
+def addToFile(path, info, mode):
+    with open(path + '.txt',f'{mode}',encoding='utf-8') as file:
+        for i in info:
+            file.write(i + '\n')
 
+def checkLog(id):
+    log = getFile('log')
+    return str(id) in log
 
-# get user id
-# 
-def checkRoom(id):
-    log_list = []
-    with open('log.txt', 'r', encoding='utf-8') as log:
-        while True:
-            a = log.readline()
-            if a != '':
-                log_list.append(a[0:len(a) - 1])
-            else:
-                break
-    for i in log_list:
+def getHost(id):
+    log = getFile('log')
+    for i in log:
         if str(id) in i:
+            line_with_id = i.split('*')
+            if line_with_id[2] =='HOST':
+                return True
+            else:
+                return False
+        else:
             return False
-            break
-    else:
-        return True
-    # return log_list
+
+def getRoom(id):
+    log = getFile('log')
+    for i in log:
+        if str(id) in i:
+            line_with_id = i.split('*')
+            room = line_with_id[1]
+            return room
+        else:
+            return False
+
+def getAnotherPlayerId(id):
+    room = getRoom(id)
+    log = getFile('log')
+    for i in log:
+        if room in i and str(id) not in i:
+            line_with_id = i.split('*')
+            another_id = line_with_id[0]
+            return another_id
+        else:
+            return False
+
+
+
+
+
+
 
 # метод принимает лист, содержащий все слова из файла (результат getThemes)
 # метод вернет кортеж с двумя листами слов (eng и rus) из одной темы
@@ -51,26 +81,15 @@ def separate(all):
 # метод принимает два листа (eng и rus) из одной темы (результат separate)
 # метод вернет кортеж из 2 листов 5 рандомных eng и 5 рандомных rus
 def random_ten_words(from_sep):
-    word_eng = []
-    word_rus = []
+    word_eng = ''
+    word_rus = ''
     while len(word_eng) != 5:
         ind = random.randint(1, len(from_sep[0])-1)
         if from_sep[0][ind] not in word_eng:
-            word_eng.append(from_sep[0][ind])
-            word_rus.append(from_sep[1][ind])
+            word_eng += from_sep[0][ind] + '*'
+            word_rus += from_sep[1][ind] + '*'
+
     return word_eng, word_rus
-
-# метод не принимает ничего
-# метод вернет 5 рандомных тем
-def get_five_themes():
-    themes = []
-    while len(themes) != 5:
-        c = random.choice(getFile('Themes\\темы'))
-        if c not in themes:
-            themes.append(c)
-    return themes
-
-
 
 # метод принимает список из 5 тем
 # метод вернет лист с 5 картами, в каждой карте есть: Тема, 5 слов на eng и 5 слов на rus
@@ -81,55 +100,38 @@ def set_cards(themes):
         cards.append(Card(themes[i], words[0], words[1]))
     return cards
 
+def get_cards(id):
+    room = getRoom(id)
+    cards = []
+    count = 0
+    while len(cards) < 5:
+        cards.append(getFile(f'Rooms\\{room}\\card{count}'))
 
-# метод вернет либо фолс, если вы не хост, либо номер комнаты, если вы хост
-def getHost(id):
-    log_list = []
-    with open('log.txt', 'r', encoding='utf-8') as log:
-        while True:
-            a = log.readline()
-            log_list = a.split('*')
-            if log_list[0] == str(id) and log_list[2][:4] == 'HOST':
-                return log_list[1]
-                break
-            elif a == '':
-                return False
-                break
 
-def getRoom(id):
-    with open('log.txt', 'r', encoding='utf-8') as log:
-        while True:
-            a = log.readline()
-            log_list = a.split('*')
-            if log_list[0] == str(id):
-                return log_list[1]
-                break
-            elif a == '':
-                return False
-                break
 
 # check player`s status
 def getStatus(player_id):
     room = getRoom(player_id)
-    print('получили письмо')
-    if getHost(id) == False:
-        path_for_create = f'{room}\\pl2'
+    # print(getHost(id))
+    if os.path.exists(f'Rooms\\{room}\\pl1.txt') or os.path.exists(f'Rooms\\{room}\\pl1.txt'):
+        if getHost(player_id) == room:
+            path_for_create = f'{room}\\pl1'
+        else:
+            path_for_create = f'{room}\\pl2'
+        with open('Rooms\\' + path_for_create + '.txt', 'r', encoding='utf-8') as file:
+            s = file.readline()
+            pl = s.split('*')
+        return pl[1]
     else:
-        path_for_create = f'{room}\\pl1'
-    with open('Rooms\\' + path_for_create + '.txt', 'r', encoding='utf-8') as file:
-        s = file.readline()
-        pl = s.split('*')
-    return pl[1]
+        return False
 
-
-
-# creating player file
-def create_pl(room_pl, player):
-    with open('Rooms\\' + room_pl + '.txt', 'w', encoding='utf-8') as file:
-        file.write(str(player[0]) + '\n')
-        file.write(str(player[1]) + '\n')
-        file.write(str(player[2]) + '\n')
-        file.write(str(player[3]) + '\n')
+# set True
+def setStatus(path):
+    player = getFile(path)
+    first_line = player[0].split('*')
+    first_line[1] = 'True'
+    player[0] = '*'.join(first_line)
+    # create_pl(path[6:], player)
 
 # set player`s cur word and status
 def setCur(pl, eng, rus, status):
@@ -139,19 +141,17 @@ def setCur(pl, eng, rus, status):
     first_line[1] = str(status)
     player[0] = '*'.join(first_line)
     player[1] = f'{eng}*{rus}*'
-    with open(pl + '.txt', 'w', encoding='utf-8') as file:
-        file.write(str(player[0]) + '\n')
-        file.write(str(player[1]) + '\n')
-        file.write(str(player[2]) + '\n')
-        file.write(str(player[3]) + '\n')
+    addToFile(pl,player,'w')
 
-# chek if correct
+# chek answer and set it done or undone
 def check(id, mes):
     room = getRoom(id)
     if getHost(id) == False:
         player = getFile(f'Rooms\\{room}\\pl2')
         path_for_create = f'{room}\\pl2'
+        setStatus(f'Rooms\\{room}\\pl1')
     else:
+        setStatus(f'Rooms\\{room}\\pl2')
         player = getFile(f'Rooms\\{room}\\pl1')
         path_for_create = f'{room}\\pl1'
     first_line = player[0].split('*')
@@ -170,4 +170,4 @@ def check(id, mes):
         undone_words.append(mes)
         player[1] = 'None*None*'
         player[3] = '*'.join(undone_words)
-    create_pl(path_for_create, player)
+    # create_pl(path_for_create, player)
